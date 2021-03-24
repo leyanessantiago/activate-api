77b0df72562ec8dfd81e2661a8bee1771e02f18d
ReneBP <rene.betancourt@savvyhawk.net>
2021-03-24 10:44:36 -0500

    #2 [Rene] Adding authentication - Adding User Module - Adding Auth Module - Adding jwt to api - Updating packages

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { SigUpValidationPipe } from './validators/sign-up-validator';
+import { JwtModule } from '@nestjs/jwt';
+import { LoginValidator } from './validators/login-validator';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtStrategy } from './jwt/jwt.startegy';
+import { JwtAuthGuard } from './jwt/jwt.guard';
+
+@Module({
+  imports: [forwardRef(() => UserModule),
+            PrismaModule,
+            JwtModule.register({
+              secret: process.env['JWT_SECRET'],
+              signOptions: { expiresIn: '8640s' },
+            })],
+  controllers: [AuthController],
+  providers: [AuthService,
+              UserService,
+              PrismaService,
+              SigUpValidationPipe,
+              LoginValidator,
+              JwtStrategy,
+              JwtAuthGuard],
+  exports: [JwtModule,
+            AuthService,
+            JwtStrategy, 
+            JwtAuthGuard]
+})
+export class AuthModule {}
