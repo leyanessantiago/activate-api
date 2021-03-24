19895db15fafb78b239543747b8de1b3aab30a64
ReneBP <rene.betancourt@savvyhawk.net>
2021-03-24 10:42:42 -0500

    #21 [Rene] Adding infrastructure artifacts - ExceptionFilter - ApiException and ValidationException - ApiResponse and SimpleResponse - AbstractValidator - Utilities

---
+++ src/core/exceptions/validation-exception.ts
@@ -0,0 +1,11 @@
+import { Dictionary } from 'express-serve-static-core';
+
+export class ValidationException extends Error {
+    validationErrors: Dictionary<Array<string>>;
+
+    constructor(validationErrors: Dictionary<Array<string>>) {
+        super();
+
+        this.validationErrors = validationErrors;
+    }
+}
