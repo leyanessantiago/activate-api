d3e4c30fb3161bf75854ea303edb97711cfc70b5
ReneBP <rene.betancourt@savvyhawk.net>
2021-04-08 11:08:56 -0500

    NOSTORY [Rene] Prettier thing

--- src/core/filters/exceptions-filter.ts
+++ src/core/filters/exceptions-filter.ts
@@ -1,40 +1,40 @@
-import { ExceptionFilter,
-    Catch,
-    ArgumentsHost,
-    HttpStatus,
-    HttpException } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { ApiResponse } from '../responses/api-response';
 import { ValidationException } from '../exceptions/validation-exception';
 import { ApiException } from '../exceptions/api-exception';
 
 @Catch()
 export class ExceptionsFilter implements ExceptionFilter {
-    catch(exception: any, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse();
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
 
-        if (exception instanceof ValidationException) {
-            response.status(HttpStatus.BAD_REQUEST).json(
-                ApiResponse.ValidationErrors(exception.validationErrors));
+    if (exception instanceof ValidationException) {
+      response
+        .status(HttpStatus.BAD_REQUEST)
+        .json(ApiResponse.ValidationErrors(exception.validationErrors));
+    } else if (exception instanceof ApiException) {
+      response
+        .status(exception.statusCode)
+        .json(ApiResponse.Fail(exception.statusCode, exception.message));
+    } else {
+      const status =
+        exception instanceof HttpException
+          ? exception.getStatus()
+          : HttpStatus.INTERNAL_SERVER_ERROR;
 
-        } else if (exception instanceof ApiException) {
-            response.status(exception.statusCode).json(
-                ApiResponse.Fail(exception.statusCode, exception.message));
-                
-        } else {
-            const status =
-            exception instanceof HttpException
-            ? exception.getStatus()
-            : HttpStatus.INTERNAL_SERVER_ERROR;
+      const errorMessage =
+        status === HttpStatus.INTERNAL_SERVER_ERROR
+          ? 'Oops something went wrong, please try later.'
+          : exception.response.message;
 
-            const errorMessage = 
-                status === HttpStatus.INTERNAL_SERVER_ERROR 
-                ? 'Oops something went wrong, please try later.'
-                : exception.response.message;
-
-            response.status(status).json(
-                ApiResponse.Fail(status, errorMessage));
-        }
-       
+      response.status(status).json(ApiResponse.Fail(status, errorMessage));
     }
-} 
+  }
+}
