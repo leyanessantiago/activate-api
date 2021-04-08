24494bfbbccc79d2e0fec8a290714b37c9054b2f
ReneBP <rene.betancourt@savvyhawk.net>
2021-04-08 11:18:37 -0500

    #9 [Rene] Adding events module

---
+++ src/event/event.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Event } from '.prisma/client';
+
+@Injectable()
+export class EventService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  public async findEventsByPublisherId(id: string): Promise<Event[]> {
+    return await this.prismaService.event.findMany({
+      where: {
+        authorId: id,
+      },
+      include: {
+        category: true,
+      },
+    });
+  }
+}
