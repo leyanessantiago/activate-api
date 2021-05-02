a52328a5c33b54432f5391b978be8d19ac8a8dca
ReneBP <rene.betancourt@savvyhawk.net>
2021-05-02 16:06:02 -0500

    #39 [Rene] Sending user info

--- src/auth/models/user-info.ts
+++ src/auth/models/user-info.ts
@@ -3,11 +3,19 @@ import { IUserInfo } from './iuser-info';
 export class UserInfo {
   sub: string;
   email: string;
+  userName: string;
+  fullName: string;
+  avatarUrl: string;
+  isVerified: boolean;
   accessToken: string;
 
   constructor(userInfo: IUserInfo) {
     this.sub = userInfo.sub;
     this.email = userInfo.email;
+    this.fullName = userInfo.fullName;
+    this.userName = userInfo.userName;
+    this.avatarUrl = userInfo.avatarUrl;
+    this.isVerified = userInfo.isVerified;
     this.accessToken = userInfo.accessToken;
   }
 }
