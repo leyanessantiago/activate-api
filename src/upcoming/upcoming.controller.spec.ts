f6e637c8abd0868189654f975a33eaa8fbe40566
luis <le.yanessantiago@gmail.com>
2021-06-17 15:14:48 -0400

    [Luis] 7: Fixing unit test.

--- src/upcoming/upcoming.controller.spec.ts
+++ src/upcoming/upcoming.controller.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UpcomingController } from './upcoming.controller';
+import { UpcomingService } from './upcoming.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UpcomingController', () => {
   let controller: UpcomingController;
 
   beforeEach(async () => {
+    const prismaServiceMock = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UpcomingController],
+      providers: [
+        UpcomingService,
+        { provide: PrismaService, useValue: prismaServiceMock },
+      ],
     }).compile();
 
     controller = module.get<UpcomingController>(UpcomingController);
