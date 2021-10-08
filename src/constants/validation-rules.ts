4ed2e47ae3c2343f40f59cddcc3af1583c209072
ayanes <alejandro.yanes94@gmail.com>
2021-10-07 23:18:16 -0400

    [Alejandro] #120: changed the regex used form the password

--- src/constants/validation-rules.ts
+++ src/constants/validation-rules.ts
@@ -3,5 +3,5 @@ import { passwordRegex } from './regex-collection';
 export const passwordValidation = {
   regex: passwordRegex,
   message:
-    'The password must have an upper case and a lower case letter, a number and a special character.',
+    'The password must have an upper case and a lower case letter, a number and be from 8 to 16 characters.',
 };
